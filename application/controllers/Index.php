afc49933a98bab7d2100a427b79d62b8b7359a15
Andrey Kudakov <kudakovandrey@gmail.com>
2017-09-25 22:03:38 +0300

    Version 1.0

---
+++ application/controllers/Index.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Andrey
+ * Date: 25.09.2017
+ * Time: 21:40
+ */
+class Index extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index(){
+        $this->load->view('index');
+    }
+}
